803e1ea941c4fdc21e401ed8c0d51c9fc60d5340
harry <tl9434@naver.com>
2024-12-19 14:45:45 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Input from './input/Input'
+import Resume from './output/resume'
+
+function App() {
+
+  return (
+    <div>
+      <Input />
+      <Resume />
+    </div>
+  )
+}
+
+export default App
