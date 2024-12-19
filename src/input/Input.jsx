0686785b7b474b24c9f05dd5b2a0f1129c1057d3
harry <tl9434@naver.com>
2024-12-19 17:55:47 +0900

    fix

--- src/input/Input.jsx
+++ src/input/Input.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import "./input.css";
+import "./input.css"
 import PersonalDetails from "./personalDetails/PersonalDetails";
 import Education from "./education/Education";
 import Experience from "./experience/Experience";
