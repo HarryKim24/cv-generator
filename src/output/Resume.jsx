23a0882f194d710ee84965d45fa86b3e70cdb8b4
harry <tl9434@naver.com>
2024-12-19 18:30:28 +0900

    fixed

--- src/output/Resume.jsx
+++ src/output/Resume.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import "../output/resume.css"
+import "../../src/output/resume.css"
 
 const Resume = ({ formData }) => {
   const { personalDetails, educations, experience, skills } = formData;
